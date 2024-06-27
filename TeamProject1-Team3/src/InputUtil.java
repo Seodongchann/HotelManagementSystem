89f220e1ee9ce0d21f5c1847ce387c39d8262496
Hak-Seok Lee <fantasytotal@naver.com>
2024-06-27 10:42:51 +0900

    10:42

--- TeamProject1-Team3/src/InputUtil.java
+++ TeamProject1-Team3/src/InputUtil.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
 public class InputUtil {
-    public static final int INPUT_TYPE_NULL = 0;
-    public static final int INPUT_TYPE_STRING = 1;
-    public static final int INPUT_TYPE_INT = 2;
-    public static final int INPUT_TYPE_DOUBLE = 3;
-    private static String buffer;
-    public static int inputType = INPUT_TYPE_NULL;
+	public static final int INPUT_TYPE_NULL = 0;
+	public static final int INPUT_TYPE_STRING = 1;
+	public static final int INPUT_TYPE_INT = 2;
+	public static final int INPUT_TYPE_DOUBLE = 3;
+	private static String buffer;
+	public static int inputType = INPUT_TYPE_NULL;
 
-    public static void inputBuffer(Scanner scanner) {
-        buffer = scanner.nextLine();
-    }
+	public static void inputBuffer(Scanner scanner) {
+		buffer = scanner.nextLine();
+	}
 
-    public static int inputInt(Scanner scanner) {
-        buffer = scanner.nextLine();
-        return 1;
-    }
+	public static int inputInt(Scanner scanner) {
+		buffer = scanner.nextLine();
+		return 1;
+	}
 
-    public static int getBufferToInt(Scanner scanner) {
-        return 1;
-    }
-    
-    
+	public static int getBufferToInt(Scanner scanner) {
+		return 1;
+	}
+
 }
